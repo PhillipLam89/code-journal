c701b8bd3a003ef2c8e29631ad5fa2ee551fd39e
Phillip Lam <plam89@ucla.edu>
2020-10-26 17:15:39 -0700

    added submit listener to form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var $avatarUrl = document.querySelector('#avatarUrl');
 var $imageSelect = document.querySelector('img');
+var $formSelect = document.querySelector('.submit-button');
+
 $avatarUrl.addEventListener('input', function (event) {
   $imageSelect.setAttribute('src', event.target.value);
+});
+
+$formSelect.addEventListener('submit', function (event) {
+  event.preventDefault();
 
 });
 
