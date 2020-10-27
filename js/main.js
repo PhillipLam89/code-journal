99c3d9989a3cab81464a8f393ee008820cbd3bb8
Phillip Lam <plam89@ucla.edu>
2020-10-26 17:47:09 -0700

    added form values to data after submit and cleared form and avatar afterwards

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var $avatarUrl = document.querySelector('#avatarUrl');
 var $imageSelect = document.querySelector('img');
-var $formSelect = document.querySelector('.submit-button');
+var $formSelect = document.querySelector('.form2');
+var $userNameInfo = document.querySelector('.user-name');
+var $userFullNameInfo = document.querySelector('.full-name');
+var $userLocationInfo = document.querySelector('.location');
+var $userBioInfo = document.querySelector('textarea');
 
 $avatarUrl.addEventListener('input', function (event) {
   $imageSelect.setAttribute('src', event.target.value);
@@ -8,7 +12,13 @@ $avatarUrl.addEventListener('input', function (event) {
 
 $formSelect.addEventListener('submit', function (event) {
   event.preventDefault();
-
+  data.profile.username = $userNameInfo.value;
+  data.profile.fullName = $userFullNameInfo.value;
+  data.profile.location = $userLocationInfo.value;
+  data.profile.bio = $userBioInfo.value;
+  document.querySelector('.form').reset();
+  document.querySelector('.form2').reset();
+  $imageSelect.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
 
 //  src="../../../jeffm/amazon return label.png"
