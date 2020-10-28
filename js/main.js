9566b5fbbb88938cbbfb6c918f3063b2d04a8975
Phillip Lam <plam89@ucla.edu>
2020-10-27 20:33:53 -0700

    finished dom tree creation and fixed css

--- js/main.js
+++ js/main.js
@@ -33,3 +33,54 @@ if (previousData !== null) {
 window.addEventListener('beforeunload', function () {
   localStorage.setItem('UserData', JSON.stringify(currentData));
 });
+
+function domTreeRender(data) {
+  var $viewProfileDiv = document.createElement('div');
+  $viewProfileDiv.setAttribute('class', 'view-profile');
+  $viewProfileDiv.setAttribute('data-view', 'view-profile');
+  var loadStorage = localStorage.getItem('UserData');
+  var newData = JSON.parse(loadStorage);
+  var $topMainAttribute = document.querySelector('.top-main');
+  var $topRow = document.querySelector('.top-row');
+
+  $viewProfileDiv.textContent = newData.fullName;
+  $topMainAttribute.appendChild($viewProfileDiv);
+
+  var $image = document.createElement('img');
+  $image.setAttribute('src', newData.avatarUrl);
+  $topMainAttribute.appendChild($image);
+
+  var $secondMainDiv = document.createElement('main');
+  $secondMainDiv.setAttribute('class', 'column-half right-column view-profile-info');
+
+  var $icon1 = document.createElement('img');
+  var $icon2 = document.createElement('img');
+  var $span1 = document.createElement('p');
+  var $span2 = document.createElement('p');
+
+  $span1.textContent = newData.username;
+  $span2.textContent = newData.location;
+
+  $icon1.setAttribute('src', '../../lfz/user Icon.png');
+  $icon1.setAttribute('class', 'icon icon1');
+
+  $icon2.setAttribute('src', '../../lfz/location-icon.jpg');
+  $icon2.setAttribute('class', 'icon icon2');
+
+  $secondMainDiv.appendChild($icon1);
+  $secondMainDiv.appendChild($span1);
+
+  $secondMainDiv.appendChild($icon2);
+  $secondMainDiv.appendChild($span2);
+
+  $topRow.appendChild($topMainAttribute);
+  $topRow.appendChild($secondMainDiv);
+
+  var $article = document.createElement('article');
+  $article.textContent = newData.bio;
+  $secondMainDiv.append($article);
+
+  return $topRow;
+}
+
+domTreeRender(data);
